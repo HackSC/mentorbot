2ae7c8f117ad3e598a3d22d7dee1cf05e7221307
Sang Woon Lee <sangwoon@usc.edu>
2017-02-07 18:15:07 -0800

    First commit

---
+++ index.js
@@ -0,0 +1,12 @@
+var slackTerminal = require('slack-terminalize');
+
+slackTerminal.init('xoxb-139124779623-NIh5HG2a7wVCEXnTkiwkDSo2', {
+    // slack rtm client options here
+    // more info at: https://github.com/slackhq/node-slack-client/blob/master/lib/clients/rtm/client.js
+}, {
+    // app configurations to suit your project structure
+    // to see the list of all possible config,
+    // check this out: https://github.com/ggauravr/slack-terminalize/blob/master/util/config.js
+	CONFIG_DIR: __dirname + '/config',
+	COMMAND_DIR: __dirname + '/commands'
+});

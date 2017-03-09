3e0594bd04a7b4a31a0295d5c6f542faa95ea427
Justin He <hejustin@usc.edu>
2017-03-08 22:57:29 -0800

    pushed basic setup for python

--- mentorbot.py
+++ mentorbot.py
@@ -1,15 +1,53 @@
-# config
+import os
+import time
 from slackclient import SlackClient
 
-slack_token = os.environ["SLACK_API_TOKEN"]
-sc = SlackClient(slack_token)
+# starterbot's ID as an environment variable
+BOT_ID = os.environ.get("BOT_ID")
+
+# constants used
+AT_BOT = "mentorbot"
+EXAMPLE_COMMAND = "/<command>"
+
+# instantiate Slack client
+slack_client = SlackClient(os.environ.get('SLACK_BOT_TOKEN'))
+
+def handle_command(command, channel):
+    """
+        Receives commands directed at the bot and determines if they
+        are valid commands. If valid, reply. If not, return default resposne.
+    """
+    print(slack_client.api_call("users.list"))
+    response = "Not sure what you mean. Use the *" + EXAMPLE_COMMAND + \
+               "* command with numbers, delimited by spaces."
+    if command.startswith(EXAMPLE_COMMAND):
+        response = "Sure...write some more code then I can do that!"
+    slack_client.api_call("chat.postMessage", channel=channel,
+                          text=response, as_user=True)
 
 
-# some test functions -- learning and playing with python-slackclient
-print(sc.api_call("channels.list"))
+def parse_slack_output(slack_rtm_output):
+    """
+        This function handles each message sent in a slack channel
+        to see if the user is sending a message referring to a bot
+    """
+    output_list = slack_rtm_output
+    if output_list and len(output_list) > 0:
+        for output in output_list:
+            if output and 'text' in output and AT_BOT in output['text']:
+                # return text after the @ mention, whitespace removed
+                return output['text'].split(AT_BOT)[1].strip().lower(), \
+                       output['channel']
+    return None, None
 
-sc.api_call(
-  "chat.postMessage",
-  channel="#dev-mentor-slackbot",
-  text="Hello from my python bot huehue --justin"
-)
+if __name__ == "__main__":
+    READ_WEBSOCKET_DELAY = 1 # 1 second delay between reading from firehose
+    if slack_client.rtm_connect():
+        print("StarterBot connected and running!")
+        while True:
+            command, channel = parse_slack_output(slack_client.rtm_read())
+            if command and channel:
+                handle_command(command, channel)
+            time.sleep(READ_WEBSOCKET_DELAY)
+    else:
+        print("Connection failed. Invalid Slack token or bot ID?")

6485f3e8b298ce81d961057ccb03695721a95ff5
Jamin Chen <jamin.chen587@gmail.com>
2017-03-09 23:22:26 -0800

    Trimmed code and added functions for buttons

--- mentorbot.py
+++ mentorbot.py
@@ -1,61 +1,70 @@
-from bottle import route, run
+from bottle import route, run, get, post, request
 import os
 import time
 from slackclient import SlackClient
 
 # starterbot's ID as an environment variable
-BOT_ID = os.environ.get("BOT_ID")
-
-# constants used
-AT_BOT = "mentorbot"
-EXAMPLE_COMMAND = "/<command>"
+#BOT_ID = os.environ.get("BOT_ID")
 
 # instantiate Slack client
-slack_client = SlackClient(os.environ.get('SLACK_BOT_TOKEN'))
+sc = SlackClient(os.environ.get('SLACK_BOT_TOKEN'))
+
+@post('/mentor')
+def mentor():
+    """
+    Usage: /mentor [category]
+    Requests a mentor for a particular category.
+    """
+    category = request.text
+    user = request.user_name
+    requestText = user + " is looking for a mentor for " + category + "! "
+    sendTextMessage("#mentors", requestText)
 
 @route('/hello')
 def hello():
     return "<h1>Hello World!</h1>"
 
-def handle_command(command, channel):
-    """
-        Receives commands directed at the bot and determines if they
-        are valid commands. If valid, reply. If not, return default resposne.
-    """
-    print(slack_client.api_call("users.list"))
-    response = "Not sure what you mean. Use the *" + EXAMPLE_COMMAND + \
-               "* command with numbers, delimited by spaces."
-    if command.startswith(EXAMPLE_COMMAND):
-        response = "Sure...write some more code then I can do that!"
-    slack_client.api_call("chat.postMessage", channel=channel,
-                          text=response, as_user=True)
+def sendTextMessage(channel, text):
+    return sc.api_call(
+        "chat.postMessage",
+        channel=channel,
+        text=text,
+        as_user=True
+    )
 
-
-def parse_slack_output(slack_rtm_output):
-    """
-        This function handles each message sent in a slack channel
-        to see if the user is sending a message referring to a bot
-    """
-    output_list = slack_rtm_output
-    if output_list and len(output_list) > 0:
-        for output in output_list:
-            if output and 'text' in output and AT_BOT in output['text']:
-                # return text after the @ mention, whitespace removed
-                return output['text'].split(AT_BOT)[1].strip().lower(), \
-                       output['channel']
-    return None, None
+def sendMentorConfirm(channel, text):
+    return sc.api_call(
+        "chat.postMessage",
+        channel=channel,
+        text=text,
+        as_user=True,
+        attachments=
+        [
+            {
+                "text":"Choose an action",
+                "fallback":"You are unable to choose an option",
+                "callback_id":"lunch_intro",
+                "color":"#3AA3E3",
+                "attachment_type":"default",
+                "actions":[
+                    {
+                        "name":"mentor confirm",
+                        "text":"Yay",
+                        "type":"button",
+                        "value":True
+                    },
+                    {
+                        "name":"mentor confirm",
+                        "text":"Nay",
+                        "type":"button",
+                        "value":False
+                    }
+                ]
+            }
+        ]
+    )
 
 if __name__ == "__main__":
-    # activates and runs the server 
+    # activates and runs the server
+    sendMentorConfirm("#bot_testing", "hello")
     run(host='0.0.0.0', port=8080)
-    READ_WEBSOCKET_DELAY = 1 # 1 second delay between reading from firehose
-    if slack_client.rtm_connect():
-        print("StarterBot connected and running!")
-        while True:
-            command, channel = parse_slack_output(slack_client.rtm_read())
-            if command and channel:
-                handle_command(command, channel)
-            time.sleep(READ_WEBSOCKET_DELAY)
-    else:
-        print("Connection failed. Invalid Slack token or bot ID?")
-

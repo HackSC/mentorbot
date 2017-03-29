a858aeab31f2bd46400cb8734e6fcd04236a473e
Jamin Chen <jamin.chen587@gmail.com>
2017-03-29 00:53:10 -0700

    testing

--- mentorbot.py
+++ mentorbot.py
@@ -71,7 +71,7 @@ def setMentorChannel():
     mentorChannel = request.forms.get("text")
     user_id = request.forms.get("user_id")
     channel_id = request.forms.get("channel_id")
-    channelsList = sc.api_call(channels.list)
+    channelsList = sc.api_call("channels.list")
     print (channelsList)
     if mentorChannel not in channelsList:
         sendTextMessage(channel_id, "The channel *" + mentorChannel + "* does not exist!")
